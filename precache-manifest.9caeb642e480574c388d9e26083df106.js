f0ddf6d61cc537cb1a80877c73f6a3010e93ead0
pabloc33 <68171460+pabloc33@users.noreply.github.com>
2020-08-25 14:14:24 -0300

    Updates

---
+++ precache-manifest.9caeb642e480574c388d9e26083df106.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e8dc48cc24216f8a5c6f8ce84e0aaf",
+    "url": "/04-gif-expert-app/index.html"
+  },
+  {
+    "revision": "3cf4a2c97de8edec2388",
+    "url": "/04-gif-expert-app/static/css/main.e0f45a58.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/04-gif-expert-app/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/04-gif-expert-app/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cf4a2c97de8edec2388",
+    "url": "/04-gif-expert-app/static/js/main.82daaaf1.chunk.js"
+  },
+  {
+    "revision": "2a84d61969e79d013f49",
+    "url": "/04-gif-expert-app/static/js/runtime-main.b0935dcc.js"
+  }
+]);
